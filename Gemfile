172db99e2184bedff2a5b0141b84bc9fe1f01bf0
Josh Rendek <josh@bluescripts.net>
2012-07-26 22:03:46 -0400

    first

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.6'
+gem 'mysql2'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem 'capistrano', :group => :development
+gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tester::Application.config.secret_token = '685a066eee2bdb5c9b9533fd0400be71705c778cda2b421eb034bcc4a358e983616d42b3f777acfb66a45b14c61c8d83fb2c44627d51a5ff26d46b62369cdd7d'
